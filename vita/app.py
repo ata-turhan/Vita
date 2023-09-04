73b2db312526f3874124d2708ef966f184ebb796
fotino21 <ataturhan21@gmail.com>
2023-09-04 11:29:48 +0300

    feature: first voice cloned with preset voices

--- vita/app.py
+++ vita/app.py
@@ -1,11 +1,14 @@
 from transformers import AutoProcessor, BarkModel
+import scipy
 
 processor = AutoProcessor.from_pretrained("suno/bark")
 model = BarkModel.from_pretrained("suno/bark")
 
-voice_preset = "v2/en_speaker_6"
+voice_preset = "v2/tr_speaker_4"
 
-inputs = processor("Hello, my dog is cute", voice_preset=voice_preset)
+inputs = processor("Bu gerçekten de işe yarıyor mu?", voice_preset=voice_preset)
 
 audio_array = model.generate(**inputs)
 audio_array = audio_array.cpu().numpy().squeeze()
+sample_rate = model.generation_config.sample_rate
+scipy.io.wavfile.write("output.wav", rate=sample_rate, data=audio_array)
