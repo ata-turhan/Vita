1195b8deab0ef7883687b7fcad1214a4592f350a
fotino21 <ataturhan21@gmail.com>
2023-09-08 16:17:51 +0300

    feature: coqui models output are very bad

--- vita/app.py
+++ vita/app.py
@@ -6,7 +6,13 @@ from TTS.api import TTS
 # Get device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+
 # List available 🐸TTS models and choose the first one
+model_names = TTS().list_models()
+for model in model_names:
+    print(model)
+
+# model_name = "tts_models/multilingual/multi-dataset/your_tts"
 model_name = "tts_models/multilingual/multi-dataset/bark"
 # Init TTS
 tts = TTS(model_name).to(device)
@@ -17,7 +23,7 @@ tts = TTS(model_name).to(device)
 # Text to speech to a file
 tts.tts_to_file(
     text="Hello world, I am ata turhan. I am speaking english!",
-    speaker_wav="bark_voices/speaker/ata.wav",
+    speaker_wav="bark_voices/speaker/ata-88 seconds.wav",
     # language="en",
-    file_path="output.wav",
+    file_path="output-88 seconds.wav",
 )
