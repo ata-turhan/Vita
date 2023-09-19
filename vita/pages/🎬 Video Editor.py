a1c0664b59329576f54ac294887bb6aac1af074e
fotino21 <ataturhan21@gmail.com>
2023-09-19 14:09:53 +0300

    feature: template for video and image editors set

---
+++ vita/pages/🎬 Video Editor.py
@@ -0,0 +1,30 @@
+import os
+import re
+import streamlit as st
+from modules.utils import add_bg_from_local, set_page_config, local_css
+
+
+def main():
+    set_page_config()
+
+    local_css()
+
+    background_img_path = os.path.join("static", "background", "Toolbox Logo.png")
+    sidebar_background_img_path = os.path.join(
+        "static", "background", "Lila Gradient.png"
+    )
+    page_markdown = add_bg_from_local(
+        background_img_path=background_img_path,
+        sidebar_background_img_path=sidebar_background_img_path,
+    )
+    st.markdown(page_markdown, unsafe_allow_html=True)
+
+    st.markdown(
+        """<h1 style='text-align: center; color: black; font-size: 40px;'> Welcome to Video Editor 🎬 </h1> \
+        <br>""",
+        unsafe_allow_html=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
