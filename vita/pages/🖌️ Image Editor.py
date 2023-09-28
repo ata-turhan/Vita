da4a97429f1319aaac7d9339ff4e10c10eafb440
fotino21 <ataturhan21@gmail.com>
2023-09-28 14:24:57 +0300

    feature: image editor first features ready

--- vita/pages/🖌️ Image Editor.py
+++ vita/pages/🖌️ Image Editor.py
@@ -2,7 +2,7 @@ import os
 
 import streamlit as st
 from modules.utils import add_bg_from_local, local_css, set_page_config
-from PIL import Image
+from PIL import Image, ImageEnhance, ImageFilter
 
 
 def main():
@@ -28,10 +28,71 @@ def main():
         unsafe_allow_html=True,
     )
 
-    image = st.file_uploader("Choose a image to edit")
+    _, center_col, _ = st.sidebar.columns(3)
+    center_col.header("Settings")
+    sharp_value = st.sidebar.slider("Sharpness")
+    color_value = st.sidebar.slider("Color")
+    brightness_value = st.sidebar.slider("Brightness")
+    contrast_value = st.sidebar.slider("Contrast")
+    flip_image_value = st.sidebar.selectbox(
+        "Flip Image",
+        options=("Original", "Top->Bottom", "Left->Right", "Both"),
+    )
+
+    st.sidebar.write("Filters")
+    filter_black_and_white = st.sidebar.checkbox("Black and white")
+    filter_blur = st.sidebar.checkbox("Blur")
+
+    if filter_blur:
+        filter_blur_strength = st.sidebar.slider("Select Blur strength")
+
+    # checking color
+    if not color_value:
+        color_value = 1
+    if not brightness_value:
+        brightness_value = 1
+    if not contrast_value:
+        contrast_value = 1
+
+    image = st.file_uploader(
+        "Choose a image to edit",
+        type=["png", "jpg"],
+        accept_multiple_files=False,
+    )
     if image is not None:
-        st.image(image)
-        Image.open(image)
+        img = Image.open(image)
+        sharp = ImageEnhance.Sharpness(img)
+        edited_img = sharp.enhance(sharp_value)
+
+        color = ImageEnhance.Color(edited_img)
+        edited_img = color.enhance(color_value)
+
+        brightness = ImageEnhance.Brightness(edited_img)
+        edited_img = brightness.enhance(brightness_value)
+
+        contrast = ImageEnhance.Contrast(edited_img)
+        edited_img = contrast.enhance(contrast_value)
+
+        if flip_image_value == "Top->Bottom":
+            edited_img = edited_img.transpose(Image.FLIP_TOP_BOTTOM)
+        elif flip_image_value == "Left->Right":
+            edited_img = edited_img.transpose(Image.FLIP_LEFT_RIGHT)
+        elif flip_image_value == "Both":
+            edited_img = edited_img.transpose(Image.FLIP_TOP_BOTTOM)
+            edited_img = edited_img.transpose(Image.FLIP_LEFT_RIGHT)
+
+        if filter_black_and_white:
+            edited_img = edited_img.convert(mode="L")
+
+        if filter_blur:
+            if filter_blur_strength:
+                edited_img = edited_img.filter(
+                    ImageFilter.GaussianBlur(filter_blur_strength)
+                )
+        _, center_col, _ = st.columns(3)
+        center_col.image(
+            edited_img,
+        )
 
 
 if __name__ == "__main__":
