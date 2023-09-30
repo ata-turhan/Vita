e11643690967e9192ac0bfd35eda82fbde6dae32
fotino21 <ataturhan21@gmail.com>
2023-09-30 18:26:38 +0300

    feature: more filters, resize, rotate added to image editor and its design changed

--- vita/pages/🖌️ Image Editor.py
+++ vita/pages/🖌️ Image Editor.py
@@ -1,10 +1,19 @@
 import os
 
-import cv2
-import numpy as np
 import streamlit as st
 from modules.utils import add_bg_from_local, local_css, set_page_config
-from PIL import Image, ImageEnhance, ImageFilter
+from PIL import Image, ImageEnhance, ImageFilter, ImageOps
+from PIL.ImageFilter import (
+    CONTOUR,
+    DETAIL,
+    EDGE_ENHANCE,
+    EDGE_ENHANCE_MORE,
+    EMBOSS,
+    FIND_EDGES,
+    SHARPEN,
+    SMOOTH,
+    SMOOTH_MORE,
+)
 from removebg import RemoveBg
 
 
@@ -45,59 +54,82 @@ def main():
     with st.sidebar:
         _, center_col, _ = st.sidebar.columns(3)
         center_col.header("Settings")
-        option = st.selectbox(
-            "Options",
-            ["<Select>", "Filters", "Image Corrections", "Background Remover"],
+
+        sharp_value = st.slider("Sharpness", 0.0, 2.0, 1.0)
+        color_value = st.slider("Color")
+        brightness_value = st.slider("Brightness", 0.0, 5.0, 1.0)
+        contrast_value = st.slider("Contrast", 0.0, 2.0, 1.0)
+        rotate_value = st.slider(
+            "Rotate",
+            0,
+            360,
         )
-        if option == "Filters":
-            filter_choice = st.selectbox(
-                "Filters",
-                ["Original", "Grayscale", "Sepia", "Contour", "Sketch"],
-            )
-            if filter_choice == "Grayscale":
-                img_convert = np.array(img.convert("RGB"))
-                gray_image = cv2.cvtColor(img_convert, cv2.COLOR_RGB2GRAY)
-                edited_img = gray_image
-            elif filter_choice == "Sepia":
-                img_convert = np.array(img.convert("RGB"))
-                img_convert = cv2.cvtColor(img_convert, cv2.COLOR_RGB2BGR)
-                kernel = np.array(
-                    [
-                        [0.272, 0.534, 0.131],
-                        [0.349, 0.686, 0.168],
-                        [0.393, 0.769, 0.189],
-                    ]
-                )
-                sepia_image = cv2.filter2D(img_convert, -1, kernel)
-                edited_img = sepia_image
-            elif filter_choice == "Contour":
-                img_convert = np.array(img.convert("RGB"))
-                img_convert = cv2.cvtColor(img_convert, cv2.COLOR_RGB2BGR)
-                blur_image = cv2.GaussianBlur(img_convert, (11, 11), 0)
-                canny_image = cv2.Canny(blur_image, 100, 150)
-                edited_img = canny_image
-            elif filter_choice == "Sketch":
-                img_convert = np.array(img.convert("RGB"))
-                gray_image = cv2.cvtColor(img_convert, cv2.COLOR_RGB2GRAY)
-                inv_gray = 255 - gray_image
-                blur_image = cv2.GaussianBlur(inv_gray, (25, 25), 0, 0)
-                sketch_image = cv2.divide(
-                    gray_image, 255 - blur_image, scale=256
-                )
-                edited_img = sketch_image
+        resize_value = st.slider(
+            "Resize (px)",
+            50,
+            500,
+        )
+        flip_image_value = st.sidebar.selectbox(
+            "Flip Image",
+            options=("Original", "Top->Bottom", "Left->Right", "Both"),
+        )
 
-        elif option == "Image Corrections":
-            sharp_value = st.slider("Sharpness", 0.0, 2.0, 1.0)
-            color_value = st.slider("Color")
-            brightness_value = st.slider("Brightness", 0.0, 5.0, 1.0)
-            contrast_value = st.slider("Contrast", 0.0, 2.0, 1.0)
-            flip_image_value = st.sidebar.selectbox(
-                "Flip Image",
-                options=("Original", "Top->Bottom", "Left->Right", "Both"),
-            )
+        if not color_value:
+            color_value = 1
+        if not brightness_value:
+            brightness_value = 1
+        if not contrast_value:
+            contrast_value = 1
 
-            st.sidebar.write("Filters")
-            filter_black_and_white = st.sidebar.checkbox("Black and white")
+        if img is not None:
+            sharp = ImageEnhance.Sharpness(edited_img)
+            edited_img = sharp.enhance(sharp_value)
+
+            color = ImageEnhance.Color(edited_img)
+            edited_img = color.enhance(color_value)
+
+            brightness = ImageEnhance.Brightness(edited_img)
+            edited_img = brightness.enhance(brightness_value)
+
+            contrast = ImageEnhance.Contrast(edited_img)
+            edited_img = contrast.enhance(contrast_value)
+
+            edited_img = edited_img.rotate(rotate_value)
+
+            edited_img = edited_img.resize((resize_value, resize_value))
+
+            if flip_image_value == "Top->Bottom":
+                edited_img = edited_img.transpose(Image.FLIP_TOP_BOTTOM)
+            elif flip_image_value == "Left->Right":
+                edited_img = edited_img.transpose(Image.FLIP_LEFT_RIGHT)
+            elif flip_image_value == "Both":
+                edited_img = edited_img.transpose(Image.FLIP_TOP_BOTTOM)
+                edited_img = edited_img.transpose(Image.FLIP_LEFT_RIGHT)
+
+            st.sidebar.header("Filters")
+            if st.sidebar.checkbox("Contour"):
+                edited_img = edited_img.filter(CONTOUR)
+            if st.sidebar.checkbox("Detail"):
+                edited_img = edited_img.filter(DETAIL)
+            if st.sidebar.checkbox("Edge Enhance"):
+                edited_img = edited_img.filter(EDGE_ENHANCE)
+            if st.sidebar.checkbox("Edge enhance more"):
+                edited_img = edited_img.filter(EDGE_ENHANCE_MORE)
+            if st.sidebar.checkbox("Emboss"):
+                edited_img = edited_img.filter(EMBOSS)
+            if st.sidebar.checkbox("Find edges"):
+                edited_img = edited_img.filter(FIND_EDGES)
+            if st.sidebar.checkbox("Grayscale"):
+                edited_img = ImageOps.grayscale(edited_img)
+            if st.sidebar.checkbox("Invert colors"):
+                edited_img = ImageOps.invert(edited_img)
+            if st.sidebar.checkbox("Smooth"):
+                edited_img = edited_img.filter(SMOOTH)
+            if st.sidebar.checkbox("Smooth more"):
+                edited_img = edited_img.filter(SMOOTH_MORE)
+            if st.sidebar.checkbox("Sharpen"):
+                edited_img = edited_img.filter(SHARPEN)
+
             filter_blur = st.sidebar.checkbox("Blur")
 
             if filter_blur:
@@ -105,49 +137,20 @@ def main():
                     "Select Blur strength"
                 )
 
-            if not color_value:
-                color_value = 1
-            if not brightness_value:
-                brightness_value = 1
-            if not contrast_value:
-                contrast_value = 1
+            if filter_blur:
+                if filter_blur_strength:
+                    edited_img = edited_img.filter(
+                        ImageFilter.GaussianBlur(filter_blur_strength)
+                    )
 
-            if img is not None:
-                sharp = ImageEnhance.Sharpness(edited_img)
-                edited_img = sharp.enhance(sharp_value)
-
-                color = ImageEnhance.Color(edited_img)
-                edited_img = color.enhance(color_value)
-
-                brightness = ImageEnhance.Brightness(edited_img)
-                edited_img = brightness.enhance(brightness_value)
-
-                contrast = ImageEnhance.Contrast(edited_img)
-                edited_img = contrast.enhance(contrast_value)
-
-                if flip_image_value == "Top->Bottom":
-                    edited_img = edited_img.transpose(Image.FLIP_TOP_BOTTOM)
-                elif flip_image_value == "Left->Right":
-                    edited_img = edited_img.transpose(Image.FLIP_LEFT_RIGHT)
-                elif flip_image_value == "Both":
-                    edited_img = edited_img.transpose(Image.FLIP_TOP_BOTTOM)
-                    edited_img = edited_img.transpose(Image.FLIP_LEFT_RIGHT)
-
-                if filter_black_and_white:
-                    edited_img = edited_img.convert(mode="L")
-
-                if filter_blur:
-                    if filter_blur_strength:
-                        edited_img = edited_img.filter(
-                            ImageFilter.GaussianBlur(filter_blur_strength)
-                        )
-        elif option == "Background Remover":
-            rmbg = RemoveBg(st.secrets["removebg_api_key"], "error.log")
-            img.save("input.jpg")
-            rmbg.remove_background_from_img_file(
-                img_file_path="input.jpg",
-            )
-            edited_img = Image.open("input.jpg_no_bg.png")
+            st.sidebar.header("Background")
+            if st.sidebar.checkbox("Remove background"):
+                rmbg = RemoveBg(st.secrets["removebg_api_key"], "error.log")
+                edited_img.save("input.jpg")
+                rmbg.remove_background_from_img_file(
+                    img_file_path="input.jpg",
+                )
+                edited_img = Image.open("input.jpg_no_bg.png")
     if img is not None:
         col1, col2 = st.columns(2, gap="large")
         col1.subheader("Before")
